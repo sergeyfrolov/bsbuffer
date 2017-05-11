e2491f7513d546918e392e5a893f699e02513839
Sergey Frolov <sergey.frolov@colorado.edu>
2017-05-11 13:23:24 -0600

    Finish BSBuffer v1.0

--- bsbuffer.go
+++ bsbuffer.go
@@ -10,29 +10,32 @@ import (
 	"sync"
 )
 
+// BSBuffer:
 // B - Blocking - Read() calls are blocking.
-// S - Safe - Supports arbitrary #s of readers and writers.
-// Buffer,
+// S - Safe - Supports arbitrary amount of readers and writers.
+// Could be unblocked and turned into SBuffer.
 type BSBuffer struct {
 	sync.Mutex
-	buf        bytes.Buffer
-	r          io.PipeReader
-	w          io.PipeWriter
+	bufIn       bytes.Buffer
+	bufOut      bytes.Buffer
+	r           io.PipeReader
+	w           io.PipeWriter
 
-	readerWait chan bool
-	hasData chan bool
-	writerWait chan bool
+	hasData     chan bool
+	engineExit  chan bool
+	unblocked   bool
+
+	unblockOnce sync.Once
 }
 
-func NewBSBuffer(initialSize int) *BSBuffer {
+// Creates new BSBuffer
+func NewBSBuffer() *BSBuffer {
 	bsb := new(BSBuffer)
-
-	b := make([]byte, initialSize)
-	bsb.buf = bytes.NewBuffer(b)
 
 	bsb.r, bsb.w = io.Pipe()
 
 	bsb.hasData = make(chan bool, 1)
+	bsb.engineExit = make(chan bool)
 	go bsb.engine()
 	return bsb
 }
@@ -42,45 +45,72 @@ func (b *BSBuffer) engine() {
 		select {
 		case _ = <-b.hasData:
 			b.Lock()
-			if b.buf.Len() == 0 {
-				// empty buffer, but b.hasData -- it was closed
+			b.bufOut.ReadFrom(b.bufIn)
+			b.Unlock()
+			_, err := b.bufOut.WriteTo(b.w)
+			if b.unblocked || err != nil {
 				b.r.Close()
-				b.w.Close()
+				close(b.engineExit)
 				return
 			}
-			slice := b.buf.Bytes()
-			sliceCopy := make([]byte, len(slice))
-			copy(sliceCopy, slice)
-			b.Unlock()
-			b.w.Write(sliceCopy)
-			continue
 		}
 	}
 }
 
-
+// Reads data from the BSBuffer, blocking until a writer arrives or the BSBuffer is unblocked.
+// If the write end is closed with an error, that error is returned as err; otherwise err is EOF.
+// Supports multiple concurrent goroutines and p is valid forever.
 func (b *BSBuffer) Read(p []byte) (n int, err error) {
 	n, err = b.r.Read(p)
+	if b.unblocked && err != nil {
+		if n != 0 {
+			// Last data from blocking mode engine.
+			// Clean error and push it up
+			err = nil
+		} else {
+			// Unblocked and no data in engine.
+			// Operate as SafeBuffer
+			b.Lock()
+			n, err = b.bufOut.Read(p)
+			b.Unlock()
+		}
+	}
 	return
 }
 
+// Non-blocking write appends the contents of p to the buffer, growing the buffer as needed.
+// The return value n is the length of p; err is always nil.
+// If the buffer becomes too large, Write will panic with ErrTooLarge.
+// Supports multiple concurrent goroutines and p is safe for reuse right away.
 func (b *BSBuffer) Write(p []byte) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
 	b.Lock()
-	n, err = b.buf.Write(p)
-	select {
-	case <-b.hasData:
-	default:
+	if b.unblocked {
+		// Wait for engine to exit and operate as Safe Buffer.
+		_ = <-b.engineExit
+		n, err = b.bufOut.Write(p)
+	} else {
+		// Push data to engine and wake it up, if needed.
+		n, err = b.bufIn.Write(p)
+		select {
+		case <-b.hasData:
+		default:
+		}
 	}
 	b.Unlock()
 	return
 }
 
-// Second close is unexpected behaviour (probably panic)
-func (b *BSBuffer) Close() {
-	b.Lock()
-	close(b.hasData)
-	b.Unlock()
+// Turns BSBuffer into SBuffer: Read() is no longer blocking, but still safe.
+// Unblock() is safe to call multiple times.
+func (b *BSBuffer) Unblock() {
+	b.unblockOnce.Do(func() {
+		b.Lock()
+		b.unblocked = true
+		b.w.Close()
+		close(b.hasData)
+		b.Unlock()
+	})
 }
